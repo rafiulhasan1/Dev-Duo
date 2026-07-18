816c86a756dcebaa28f590fa33b97930e1c3108d
rafiulhasan1 <rafiulhasan001@gmail.com>
2026-07-18 15:52:42 +0600

    env file added

--- src/firebase/firebase.config.js
+++ src/firebase/firebase.config.js
@@ -3,12 +3,12 @@ import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBk1rABuN3RnIkW14aaQaaV6heAGE_B4o8",
-  authDomain: "portfolio-system-a5ab3.firebaseapp.com",
-  projectId: "portfolio-system-a5ab3",
-  storageBucket: "portfolio-system-a5ab3.firebasestorage.app",
-  messagingSenderId: "576190973725",
-  appId: "1:576190973725:web:d6cd4b23bf8511a37983f4",
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
 };
 
 const app = initializeApp(firebaseConfig);
